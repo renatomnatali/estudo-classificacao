139d4cfe5fabc69ff93aac51d4b290381df0ce5f
renatomnatali <renatomnatali@gmail.com>
2020-05-23 23:28:46 -0300

    Initial Commit

---
+++ porco-cachorro.py
@@ -0,0 +1,35 @@
+#[é gordinho, tem pern curta, .late]
+
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro4 = [1, 1, 1]
+cachorro5 = [0, 1, 1]
+cachorro6 = [0, 1, 1]
+
+dados = [porco1, porco2, porco3, cachorro4, cachorro5, cachorro6]
+
+marcacoes = [1, 1, 1, -1, -1, -1]
+
+from sklearn.naive_bayes import MultinomialNB
+
+modelo = MultinomialNB()
+modelo.fit(dados, marcacoes)
+
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
+
+teste = [misterioso1, misterioso2, misterioso3]
+
+marcacoes_teste = [-1, 1, -1]
+
+resultado = modelo.predict(teste)
+print(resultado)
+
+
+diferencas = resultado - marcacoes_teste
+
+acertos = [d for d in diferencas if d == 0]
+
+print(acertos)
